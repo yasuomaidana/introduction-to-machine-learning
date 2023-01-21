bcd06b645c12c0dc84163e3ae8fcda9946a0783e
yasuomaidana <yasuomaidana@gmail.com>
2023-01-21 18:59:15 +0900

    Check if pytorch was installed successfully

--- installing_pytorch/main.py
+++ installing_pytorch/main.py
@@ -1,16 +1,20 @@
+import torch
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def check_instalation():
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(torch.__version__)
+    print(torch.cuda.is_available())
+    x = torch.rand(2, 3)
+    print(x)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    check_instalation()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
