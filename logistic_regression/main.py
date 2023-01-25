046029f3425e3d7a08057241ecfff6920a5fc304
yasuomaidana <yasuomaidana@gmail.com>
2023-01-25 19:12:08 +0900

    Removing plt show to avoid get stuck while executing

--- logistic_regression/main.py
+++ logistic_regression/main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import torch
 from tqdm import tqdm
 from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 
 mnist_train = datasets.MNIST(root="./datasets", train=True, transform=transforms.ToTensor(), download=True)
 mnist_test = datasets.MNIST(root="./datasets", train=False, transform=transforms.ToTensor(), download=True)
@@ -18,6 +19,14 @@ print("Default image shape: {}".format(image.shape))
 image = image.reshape([28,28])
 print("Reshaped image shape: {}".format(image.shape))
 plt.imshow(image, cmap="gray")
-plt.show()
+# plt.show() If you want to see step by step, please open the jupyter notebook file
 # Print the label
 print("The label for this image: {}".format(label))
+
+train_loader = DataLoader(mnist_train, batch_size=100, shuffle=True)
+test_loader = DataLoader(mnist_test, batch_size=100, shuffle=False)
+
+data_train_iter = iter(train_loader)
+images, labels = next(data_train_iter)
+print("Shape of the minibatch of images: {}".format(images.shape))
+print("Shape of the minibatch of labels: {}".format(labels.shape))
