e87a3ab483b31a861c6de471c5625a705e469ee3
yasuomaidana <yasuomaidana@gmail.com>
2023-03-27 18:36:13 +0900

    COUR-898: Updating data handler

--- Introduction to Supervised Learning/Multiple Linear Regresion/data_downloader.py
+++ Introduction to Supervised Learning/Multiple Linear Regresion/data_downloader.py
@@ -1,10 +1,22 @@
 import requests
 import os.path
+import zipfile
 from os import path
+
+if not path.exists("data"):
+    os.mkdir("data")
 
 if not path.exists("data/auto-mpg.data"):
     print("Downloading data")
     URL = "https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data"
     response = requests.get(URL)
     open("data/auto-mpg.data", "wb").write(response.content)
+if not path.exists("data/bodyfat.csv"):
+    print("Unziping files")
+    with zipfile.ZipFile("extra_data.zip",'r') as zip_ref:
+        zip_ref.extractall("data/")
+    allfiles = os.listdir("./data/Files")
+    for i in allfiles:
+        os.rename("./data/Files/"+i,"./data/"+i)
+    os.rmdir("./data/Files")
 print("Data loaded")
