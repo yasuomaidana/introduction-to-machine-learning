d2de78accb38ea58f8af07ba6c7c723acddeb6ed
yasuomaidana <yasuomaidana@gmail.com>
2023-07-05 15:36:37 +0900

    COUR-1014: Starting project

---
+++ Deep Learning/CNN/data_loader.py
@@ -0,0 +1,31 @@
+import kaggle
+import os
+import zipfile
+
+
+def unzip_all(zip_name: str):
+    with zipfile.ZipFile(f"data/{zip_name}.zip") as zip_file:
+        zip_file.extractall(path="data/")
+
+
+def download():
+    print("Downloading files")
+    competition = "histopathologic-cancer-detection"
+    kaggle.KaggleApi().authenticate()
+    kaggle.api.competition_download_cli(competition, path="data/")
+    unzip_all(competition)
+    os.remove(f"data/{competition}.zip")
+
+
+if os.path.exists("data/"):
+    print("exists")
+    csv_files = len([f for f in os.listdir("data/") if f.endswith(".csv")])
+    if csv_files < 2:
+        download()
+
+else:
+    os.mkdir("data/")
+    download()
+
+print("Data loaded")
+
