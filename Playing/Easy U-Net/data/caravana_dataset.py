d29dffc57282600e73d4b555aaf86d5652c6d6bc
yasuomaidana <yasuomaidana@gmail.com>
2024-07-20 01:20:56 +0900

    Reducing dataset complexity

--- Playing/Easy U-Net/data/caravana_dataset.py
+++ Playing/Easy U-Net/data/caravana_dataset.py
@@ -1,43 +1,28 @@
 import os
 
 from PIL import Image
-from numpy.random import shuffle
 from torch.utils.data import Dataset
 from torchvision.transforms import Compose, Resize, ToTensor
 
 
 class CaravanDataset(Dataset):
-    def __init__(self, root_dir, directory_name="train", ratio=0.9):
+    def __init__(self, root_dir, directory_name="train"):
         self.root_dir = f"{root_dir}/{directory_name}"
         images_directory = f"{root_dir}/{directory_name}_images"
         masks_directory = f"{root_dir}/{directory_name}_masks"
-        images = sorted([f"{images_directory}/{img}" for img in os.listdir(images_directory)])
-        masks = sorted([f"{masks_directory}/{mask}" for mask in os.listdir(masks_directory)])
 
-        data = list(zip(images, masks))
-        shuffle(data)
-
-        self.eval_mode = False
-        split_index = int(ratio * len(data))
-
-        self.training_images = [img for img, _ in data[:split_index]]
-        self.training_masks = [mask for _, mask in data[:split_index]]
-        self.validation_images = [img for img, _ in data[split_index:]]
-        self.validation_masks = [mask for _, mask in data[split_index:]]
+        self.images = sorted([f"{images_directory}/{img}" for img in os.listdir(images_directory)])
+        self.masks = sorted([f"{masks_directory}/{mask}" for mask in os.listdir(masks_directory)])
 
         self.transform = Compose([Resize((512, 512)), ToTensor()])
 
     def __getitem__(self, index):
-        if self.eval_mode:
-            img, mask = self.validation_images[index], self.validation_masks[index]
-        else:
-            img, mask = self.training_images[index], self.training_masks[index]
-        img = Image.open(img).convert("RGB")
-        mask = Image.open(mask).convert("L")
+        img = Image.open(self.images[index]).convert("RGB")
+        mask = Image.open(self.masks[index]).convert("L")
         return self.transform(img), self.transform(mask)
 
     def __len__(self):
-        return len(self.training_images) if not self.eval_mode else len(self.validation_images)
+        return len(self.images)
 
 
 if __name__ == "__main__":
