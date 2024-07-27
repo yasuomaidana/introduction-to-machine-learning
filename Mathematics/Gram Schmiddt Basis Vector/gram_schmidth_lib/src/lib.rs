d67606cb0684885140264527798749a2fd5cea0d
yasuomaidana <yasuomaidana@gmail.com>
2024-07-27 14:28:20 +0900

    Implementing Gram Schmidt algorithm

--- Mathematics/Gram Schmiddt Basis Vector/gram_schmidth_lib/src/lib.rs
+++ Mathematics/Gram Schmiddt Basis Vector/gram_schmidth_lib/src/lib.rs
@@ -1,6 +1,53 @@
 mod lib_test;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[derive(Debug)]
+pub struct Basis{
+    pub rank: usize,
+    pub basis: Vec<Vec<f64>>,
+}
+
+fn dot_product(v1:&Vec<f64>, v2:&Vec<f64>) -> f64{
+    v1.iter().zip(v2.iter())
+        .map(|(x, y)|  x * y)
+        .sum()
+}
+
+fn norm(v:&Vec<f64>) -> f64{
+    v.iter().map(|x| x * x).sum::<f64>().sqrt()
+}
+
+fn norm_vector(v:&Vec<f64>) -> Vec<f64>{
+    let norm = norm(v);
+    v.iter().map(|x| x / norm).collect()
+}
+
+/// Computes the projection of vector `v1` onto vector `v2`.
+fn projection_of_vector(v1:&Vec<f64>, v2:&Vec<f64>) -> Vec<f64>{
+    let dot = dot_product(v1, v2);
+    norm_vector(&v2).iter().map(|x| x * dot).collect()
+}
+
+pub fn gram_schmidt(vectors:&Vec<Vec<f64>>) -> Basis {
+    let mut basis_vectors:Vec<Vec<f64>> = Vec::new();
+    let mut rank:usize = 0;
+    for vector in vectors.iter(){
+        if rank > 0 {
+            let mut new_vector = vector.clone();
+            for i in 0..rank{
+                let projection = projection_of_vector(&vector, &basis_vectors[i]);
+                new_vector = new_vector.iter().zip(projection.iter()).map(|(x, y)| x - y).collect();
+            }
+            if norm(&new_vector) <= 1e-6{
+                break;
+            }
+
+            basis_vectors.push(norm_vector(&new_vector));
+        }else{
+            basis_vectors.push(norm_vector(&vector));
+        }
+        rank += 1;
+    }
+
+    Basis{rank, basis:basis_vectors}
 }
 
--- Mathematics/Gram Schmiddt Basis Vector/gram_schmidth_lib/src/lib_test.rs
+++ Mathematics/Gram Schmiddt Basis Vector/gram_schmidth_lib/src/lib_test.rs
@@ -1,8 +1,15 @@
 #[cfg(test)]
 mod tests {
+    use crate::gram_schmidt;
+
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let input_vectors: Vec<Vec<f64>> = vec![
+            vec![1.0,2.0],
+            vec![2.0,0.0],
+            vec![3.0,1.0],
+        ];
+        let basis = gram_schmidt(&input_vectors);
+        println!("{:?}", basis);
     }
 }
